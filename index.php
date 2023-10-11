9f8fff56baef2645efa48b48d92f0731d7c6091b
nisi <nisi@gmail.com>
2023-10-11 15:19:32 +0000

    Index page

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
 #$output = shell_exec('echo ip-172-31-43-117.ap-south-1.compute.internal');
 echo "<h1><center><pre>$output</pre></center></h1>";
-echo "<h1><center> Website version1 - Managed by Jenkins </center></h1>"
+echo "<h1><center> Website Version1 - Managed by Jenkins </center></h1>"
 ?>
